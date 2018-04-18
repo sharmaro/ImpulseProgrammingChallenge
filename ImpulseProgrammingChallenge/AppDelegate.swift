aaa30989ddb44052f8b1cf014692d5eb24e477f7
Rohan Sharma <sharmaro@usc.edu>
2018-04-18 11:50:23 -0700

    launchscreen issue fix

--- ImpulseProgrammingChallenge/AppDelegate.swift
+++ ImpulseProgrammingChallenge/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSPlacesClient.provideAPIKey(AppDelegate.apiKey)
+//        GMSPlacesClient.provideAPIKey(AppDelegate.apiKey)
         
         return true
     }
@@ -44,7 +44,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
-
